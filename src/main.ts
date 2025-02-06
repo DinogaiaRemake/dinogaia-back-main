bbd4c9033720d407b84be39db7f42668c4acb223
Louis Villedieu <louis.villedieu@epitech.eu>
2025-02-06 16:28:58 +0100

    move cors to nginx

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     app.use(cookieParser());
-
-    const allowedOrigins = ["https://dinogaiaremake.fr", "http://localhost:3001"];
-
-    app.enableCors({
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(null, false);
-            }
-        },
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-        allowedHeaders: ["Content-Type", "Authorization", "Accept", "Cookie", "Set-Cookie"],
-        exposedHeaders: ["Set-Cookie"]
-    });
-
     await app.listen(3000, "0.0.0.0");
 }
 
