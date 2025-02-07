abda9e689adb1af71f225089786216eafcbdd669
LouisVilledieu <72012406+LouisVilledieu@users.noreply.github.com>
2025-02-07 17:35:33 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     app.use(cookieParser());
-
-    const allowedOrigins = ["https://dinogaiaremake.fr", "http://localhost:3001"];
-
-    app.use((req, res, next) => {
-        const origin = req.headers.origin;
-        if (allowedOrigins.includes(origin)) {
-            res.header("Access-Control-Allow-Origin", origin);
-            res.header("Access-Control-Allow-Credentials", "true");
-            res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
-            res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Accept, Cookie, Set-Cookie");
-        }
-        
-        if (req.method === "OPTIONS") {
-            res.status(204).end();
-        } else {
-            next();
-        }
-    });
-
     await app.listen(3000, "0.0.0.0");
 }
 
